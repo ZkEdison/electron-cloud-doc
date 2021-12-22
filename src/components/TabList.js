0e00e6946ca96aeb80e1ed85c5a2bb033e4fd365
张康 <zhangk@shanhs.com>
2021-12-22 21:28:21 +0800

    tabpane

--- src/components/TabList.js
+++ src/components/TabList.js
@@ -1,12 +1,31 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Tabs } from "antd";
+import { Tabs,Badge } from "antd";
 import SimpleMDE from "react-simplemde-editor";
+
 import "easymde/dist/easymde.min.css";
 
 const renderTabBar = (props, DefaultTabBar) => {
-  console.log(DefaultTabBar, props)
+  console.log(props)
   return (
     <DefaultTabBar {...props}></DefaultTabBar>
+  )
+}
+
+const ControlledUsage = () => {
+  const [value, setValue] = useState("Initial value");
+
+  const onChange = (value) => {
+    setValue(value);
+  };
+
+  return <SimpleMDE value={value} onChange={onChange} />;
+};
+
+const TabPane = (props) => {
+  return (
+    <Badge dot>
+      <div>12 {props.name}</div>
+    </Badge>
   )
 }
 
@@ -15,8 +34,8 @@ const TabList = () => {
   return (
     <Tabs type="editable-card" renderTabBar={renderTabBar} style={{padding: '10px'}}>
       {list.map((item, index) => (
-        <Tabs.TabPane tab={`tab-${item}`} key={index} className="tab-pane-content" style={{}}>
-          <SimpleMDE></SimpleMDE>
+        <Tabs.TabPane tab={<TabPane name={item}/>} key={index} className="tab-pane-content" style={{}}>
+          <ControlledUsage/>
         </Tabs.TabPane>
       ))}
     </Tabs>
